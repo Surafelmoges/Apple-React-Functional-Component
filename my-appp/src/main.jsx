3529634669d4eb173ab3cbfe06123cc3063d38ab
Surafelmoges <surafelmoges01@gmail.com>
2025-11-03 08:34:04 +0000

    Start Project with React

---
+++ my-appp/src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './assets/css/bootstrap-5.3.8-dist/css/bootstrap.min.css'
+//import './assets/css/bootstrap-5.3.8-dist/js/bootstrap.bundle.min.js'
+import './index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
